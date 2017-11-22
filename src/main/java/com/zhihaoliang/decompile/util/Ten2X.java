9944fe3e7a0286ac8891da1dc1b9e54046c7544c
“zhihaoliang” <zhihaoliang07@163.com>
2017-11-22 15:41:39 +0800

    debug

---
+++ src/main/java/com/zhihaoliang/decompile/util/Ten2X.java
@@ -0,0 +1,16 @@
+package com.zhihaoliang.decompile.util;
+
+/**
+ * Created by haoliang on 2017/11/22.
+ * email:zhihaoliang07@163.com
+ *
+ * @author zhihaoliang
+ *         十进制转化为16进制
+ */
+public class Ten2X {
+    public static final void main(String[] args) {
+        int num = 2130903490;
+        System.out.print(Integer.toHexString(2130903490));
+    }
+
+}
